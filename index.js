22024571e5837a7c6535994df01bf3625974d02a
RuiJin <rui.jin@sjsu.edu>
2020-06-07 20:05:53 -0700

    data create

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
-fs.writeFileSync('hello.txt','Hello from Node.js');
-fs.writeFileSync('hello.txt','Hello again from Node.js');
-const randomNum = require('./getRandomValue');
+const randomNum = require('./control/getRandomValue');
 
 const DataSize = 10000;
-const Data_Column  = "10/1/15 - 10/30/05";
+const Data_Column  = "10/1/05 - 10/30/05";
 const Rater_Column =  ['A','B','C','D','E'];
 const Correct_Answers_3_Label = ["Low","Average", "High"];
 const Correct_Answer_5_Label = ["Bad","Okay", "Intermediate", "Great","Exceptional"];
@@ -12,9 +10,23 @@ const Rater_Answer_3_Label = ["Low", "Average", "High"];
 const Rater_Answer_5_Label =["Bad", "Okay","Intermediate", "Great","Exceptional"];
 const Task_ID = "1-10000";
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const randomMonth = months[Math.floor(Math.random() * months.length)];
+const data = {};
+  for (let i= 1; i<=DataSize; i++) {
+    data["Task_ID_"+i]= {
+        TASK_ID : i,
+        Data_Column : `10/${randomNum.getRandomIntInclusive(1,30)}/05`,
+        Rater_Column : Rater_Column[Math.floor(Math.random() * Rater_Column.length)],
+        Correct_Answers_3_Label : Correct_Answers_3_Label[Math.floor(Math.random() * Correct_Answers_3_Label.length)],
+        Correct_Answer_5_Label : Correct_Answer_5_Label[Math.floor(Math.random() * Correct_Answer_5_Label.length)],
+        Rater_Answer_3_Label : Rater_Answer_3_Label[Math.floor(Math.random() * Rater_Answer_3_Label.length)],
+        Rater_Answer_5_Label : Rater_Answer_5_Label[Math.floor(Math.random() * Rater_Answer_5_Label.length)],}
+}
 
-console.log("random month =>", randomMonth);
+  for (let task in data) {
+      data[task].Label_Agreement_3 = (data[task].Correct_Answers_3_Label === data[task].Rater_Answer_3_Label);
+      data[task].Label_Agreement_5 = (data[task].Correct_Answer_5_Label === data[task].Rater_Answer_5_Label);
+  }
 
-console.log(randomNum.getRandomIntInclusive(1,30));
+console.log(data);
+fs.writeFileSync('data', JSON.stringify(data))
+
