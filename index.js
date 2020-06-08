697d752fd2d473e9d1f44290dafaef5b2ecbbd9b
RuiJin <rui.jin@sjsu.edu>
2020-06-07 21:02:43 -0700

    more

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const randomNum = require('./control/getRandomValue');
 
+/** Data Model Description **/
 const DataSize = 10000;
 const Data_Column  = "10/1/05 - 10/30/05";
 const Rater_Column =  ['A','B','C','D','E'];
@@ -10,23 +11,25 @@ const Rater_Answer_3_Label = ["Low", "Average", "High"];
 const Rater_Answer_5_Label =["Bad", "Okay","Intermediate", "Great","Exceptional"];
 const Task_ID = "1-10000";
 
-const data = {};
+/** Step1 Initialization Random Data **/
+ const data = { table: []};
   for (let i= 1; i<=DataSize; i++) {
-    data["Task_ID_"+i]= {
+    data.table.push ({
         TASK_ID : i,
-        Data_Column : `10/${randomNum.getRandomIntInclusive(1,30)}/05`,
+        Date_Column : `10/${randomNum.getRandomIntInclusive(1,30)}/05`,
         Rater_Column : Rater_Column[Math.floor(Math.random() * Rater_Column.length)],
         Correct_Answers_3_Label : Correct_Answers_3_Label[Math.floor(Math.random() * Correct_Answers_3_Label.length)],
         Correct_Answer_5_Label : Correct_Answer_5_Label[Math.floor(Math.random() * Correct_Answer_5_Label.length)],
         Rater_Answer_3_Label : Rater_Answer_3_Label[Math.floor(Math.random() * Rater_Answer_3_Label.length)],
-        Rater_Answer_5_Label : Rater_Answer_5_Label[Math.floor(Math.random() * Rater_Answer_5_Label.length)],}
-}
+        Rater_Answer_5_Label : Rater_Answer_5_Label[Math.floor(Math.random() * Rater_Answer_5_Label.length)],});
+};
 
-  for (let task in data) {
-      data[task].Label_Agreement_3 = (data[task].Correct_Answers_3_Label === data[task].Rater_Answer_3_Label);
-      data[task].Label_Agreement_5 = (data[task].Correct_Answer_5_Label === data[task].Rater_Answer_5_Label);
+/** Step2 Add extra two columns for agreement **/
+  for (let task of data.table) {
+      task.Label_Agreement_3 = (task.Correct_Answers_3_Label === task.Rater_Answer_3_Label);
+      task.Label_Agreement_5 = (task.Correct_Answer_5_Label === task.Rater_Answer_5_Label);
   }
 
 console.log(data);
-fs.writeFileSync('data', JSON.stringify(data))
+fs.writeFileSync('data.json', JSON.stringify(data))
 
